e8326fab7e3a8a0c19a5a4bc0db11dd5ad35341f
yangming <1549516057@qq.com>
2019-11-12 17:25:12 +0800

    测试提交

--- src/main/java/com/ym/javabase/designmode/singleton/SingletonEnum.java
+++ src/main/java/com/ym/javabase/designmode/singleton/SingletonEnum.java
@@ -3,8 +3,30 @@ package com.ym.javabase.designmode.singleton;
 /**
  * @author YM
  * @date 2019/11/8 15:29
+ * 枚举类单例模式
+ * <p>
+ * 枚举类型是线程安全的，并且只会装载一次，设计者充分的利用了枚举的这个特性来实现单例模式，
+ * 枚举的写法非常简单，而且枚举类型是所用单例实现中唯一一种不会被破坏的单例实现模式。
  */
 public class SingletonEnum {
-    private SingletonEnum(){
+    private SingletonEnum() {
+    }
+
+    private enum Singleton {
+        INSTANCE;
+
+        private final SingletonEnum instance;
+
+        Singleton() {
+            instance = new SingletonEnum();
+        }
+
+        private SingletonEnum getSingletonEnumInstance() {
+            return instance;
+        }
+
+        public static SingletonEnum getInstance() {
+            return Singleton.INSTANCE.getSingletonEnumInstance();
+        }
     }
 }
